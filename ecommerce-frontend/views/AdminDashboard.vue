7abd8956a75cefcdefee5c4037692d3171481766
saran <patubojohnkevin06@gmail.com>
2025-05-05 15:25:30 +0800

    Added AdminDashboard and Cart views

---
+++ ecommerce-frontend/views/AdminDashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Admin Dashboard</h1>
+    <AdminProductManagement />
+  </div>
+</template>
+
+<script>
+import AdminProductManagement from '@/components/AdminProductManagement.vue';
+
+export default {
+  components: {
+    AdminProductManagement,
+  },
+};
+</script>
---
+++ ecommerce-frontend/views/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <ShoppingCart />
+  </div>
+</template>
+
+<script>
+import ShoppingCart from '@/components/ShoppingCart.vue';
+
+export default {
+  components: {
+    ShoppingCart,
+  },
+};
+</script>
